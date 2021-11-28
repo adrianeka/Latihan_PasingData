d2886e502e0ba88b65f206f0de06734d544a4314
AdrianEka <adrian.eka9@gmail.com>
2021-11-28 13:48:08 +0700

    first commit

---
+++ app/src/main/java/com/example/passingdata/MainActivity2.java
@@ -0,0 +1,23 @@
+package com.example.passingdata;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        TextView t = findViewById(R.id.text_viewdata);
+        Bundle bundle=getIntent().getExtras();
+        String s=bundle.getString("name");
+        t.setText(s);
+    }
+}
